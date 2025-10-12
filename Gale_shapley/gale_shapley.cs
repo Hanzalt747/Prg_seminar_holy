26f5f4a7c904497cf3addf0fe852b21f1a6a454d
Hanzalt <Hanzalt747@gmail.com>
2025-10-12 18:07:59 +0200

    Reseni problemu stabilniho manzelstvi

--- Gale_shapley/gale_shapley.cs
+++ Gale_shapley/gale_shapley.cs
@@ -1,40 +1,93 @@
 /*
 	Author: Jan Holy
-	Date: 28. 9. 2025
+	Date: 12. 10. 2025
 */
 using System;
 using System.Collections.Generic;
 
-namespace gale_shapley.cs
+class StableMarriage
 {
-    internal class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        int n = int.Parse(Console.ReadLine());
+
+        // Preference žen (womenPreferences[w][i] = i-tý muž v seznamu ženy w)
+        int[][] womenPreferences = new int[n][];
+        for (int i = 0; i < n; i++)
         {
-		int n;
-		int arraySize;
-		int[,] mainArray;
-		Console.WriteLine("Napis preference (nejdriv zeny, pak muzi): ");
-		n = int.Parse(Console.ReadLine());
-		arraySize = n*n;
-		mainArray = new int[arraySize, n+n-1];
-		for (int i = 0; i < arraySize; i++) {
-			string line = Console.ReadLine();
-			for (int j = 0; j < n+n-1; j++) {
-				char cell = line[j];
-				if (!Char.IsWhiteSpace(cell)) {
-					mainArray[i,j] = cell -'0';
-				}
-			}
-		}
+            string[] line = Console.ReadLine().Split();
+            womenPreferences[i] = Array.ConvertAll(line, int.Parse);
+        }
 
-		for (int i = 0; i < arraySize; i++) {
-            		for (int j = 0; j < n+n-1; j++) {
-            			Console.Write(mainArray[i, j]);
-            		}
-            		Console.WriteLine();
-        	}
-	}
+        // Preference mužů (muž -> (žena -> priorita))
+        int[][] menPreferences = new int[n][];
+        for (int i = 0; i < n; i++)
+        {
+            string[] line = Console.ReadLine().Split();
+            menPreferences[i] = Array.ConvertAll(line, int.Parse);
+        }
 
+        // Mapa mužských preferencí pro rychlý přístup (menRank[m][w] = priorita ženy w pro muže m)
+        int[][] menRank = new int[n][];
+        for (int m = 0; m < n; m++)
+        {
+            menRank[m] = new int[n + 1]; // 1-based
+            for (int rank = 0; rank < n; rank++)
+            {
+                int woman = menPreferences[m][rank];
+                menRank[m][woman] = rank;
+            }
+        }
+
+        int[] womanNextProposal = new int[n]; // Index dalšího muže, kterého žena osloví
+        int[] manPartner = new int[n];        // manPartner[m] = index ženy + 1
+        Array.Fill(manPartner, -1);           // -1 značí, že muž je volný
+
+        bool[] engaged = new bool[n];         // Je žena zadaná?
+
+        Queue<int> freeWomen = new Queue<int>();
+        for (int i = 0; i < n; i++) freeWomen.Enqueue(i); // všechny ženy jsou na začátku volné
+
+        while (freeWomen.Count > 0)
+        {
+            int w = freeWomen.Dequeue();
+            int nextManIndex = womanNextProposal[w]++;
+            int m = womenPreferences[w][nextManIndex] - 1; // preference jsou 1-based
+
+            if (manPartner[m] == -1)
+            {
+                manPartner[m] = w;
+                engaged[w] = true;
+            }
+            else
+            {
+                int currentWoman = manPartner[m];
+                if (menRank[m][w + 1] < menRank[m][currentWoman + 1])
+                {
+                    manPartner[m] = w;
+                    engaged[w] = true;
+                    engaged[currentWoman] = false;
+                    freeWomen.Enqueue(currentWoman);
+                }
+                else
+                {
+                    freeWomen.Enqueue(w);
+                }
+            }
+        }
+
+        // Výstup: pro každou ženu, zjistíme ke kterému muži byla přiřazena
+        int[] womanToMan = new int[n]; // index ženy -> muž (1-based)
+        for (int m = 0; m < n; m++)
+        {
+            int w = manPartner[m];
+            womanToMan[w] = m + 1;
+        }
+
+        foreach (int man in womanToMan)
+        {
+            Console.WriteLine(man);
+        }
     }
 }
+
